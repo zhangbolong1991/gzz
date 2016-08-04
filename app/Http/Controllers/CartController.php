86e4d7a75d9711724640b6c1174ee2c8ca84bac0
gengld <283491378@qq.com>
2016-08-04 11:33:40 +0800

    gengld前台详情页

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -9,13 +9,17 @@ use App\Http\Controllers\Controller;
 
 class CartController extends Controller
 {
+	//加入购物车
+	public function add(){
+
+	}
     
 	//城市级联
 	public function csjl(){
 		// echo "111";
 		return view('csjl.index');
 	}
-
+	//城市级联响应数据
 	public function s(Request $request){
 		$data=DB::table('district')->where('upid','=',$request->input('upid'))->get();
 		echo json_encode($data);
